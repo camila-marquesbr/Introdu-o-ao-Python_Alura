40ee0bbf13f9078efe87d66755f95953015b76fa
camila-marquesbr <camiladev.br@gmail.com>
2021-09-17 16:24:53 -0300

    fazendo jogo da forca1

--- forca.py
+++ forca.py
@@ -3,6 +3,16 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    #enquanto não enforcou E não acertou
+    while(not enforcou and not acertou):
+        print("joando...")
+
+
     print("Fim do jogo")
 
 if(__name__ == "__main__"):
