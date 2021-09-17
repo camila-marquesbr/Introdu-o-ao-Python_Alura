46b3ca211d1e4585d02321cefc3e30211bc825b8
camila-marquesbr <camiladev.br@gmail.com>
2021-09-17 16:34:25 -0300

    encontrar letra na posicao

--- forca.py
+++ forca.py
@@ -10,7 +10,16 @@ def jogar():
 
     #enquanto não enforcou E não acertou
     while(not enforcou and not acertou):
+        chute = input("Qual a letra?")
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute ==letra):
+                print("Encontrei a letra {} na posição".format(letra, index))
+            index = index + 1
         print("joando...")
+
+
 
 
     print("Fim do jogo")
