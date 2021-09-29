625679976d53e944bb7e2046266fa54158828c8e
camila-marquesbr <camiladev.br@gmail.com>
2021-09-29 19:28:08 -0300

    escolhendo uma palavra

--- forca.py
+++ forca.py
@@ -1,7 +1,21 @@
+import random
+
+
 def jogar():
     print("*********************************")
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
+
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+
+    for linha in arquivo:
+        linha =linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+
+    numero = random.randrange(0,len(palavras))
 
     palavra_secreta = "maça".upper()
     letras_acertadas = []
